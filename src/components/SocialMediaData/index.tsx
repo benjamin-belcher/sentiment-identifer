aacac479290a4426fe379a212a0038576ca00b34
benjamin.belcher <benjamin.belcher@ideagen.com>
2022-07-20 09:30:18 +0100

    Added functionality for keywork input

--- src/components/SocialMediaData/index.tsx
+++ src/components/SocialMediaData/index.tsx
@@ -1,11 +1,14 @@
 import {
     Box,
+    Button,
+    Chip,
     FormControl,
     InputLabel,
     MenuItem,
     Paper,
     Select,
     SelectChangeEvent,
+    Stack,
     TextField,
     Typography
 } from '@mui/material';
@@ -14,11 +17,25 @@ import React from 'react';
 export default function SocialMediaData(){
     const [platform, setPlatform] = React.useState('');
     const [displayKeywordSearch, setDisplayKeywordSearch] = React.useState(false);
+    const [keywords, addKeywords] = React.useState<string[]>([]);
+    const [keywordInput, setKeywordInput] = React.useState("");
 
     const handleChange = (event: SelectChangeEvent) => {
         setPlatform(event.target.value as string);
         setDisplayKeywordSearch(true);
     };
+
+    const addKeyword = () => {
+        addKeywords(keywords.concat(keywordInput));
+        setKeywordInput("");
+    }
+
+    const removeKeyword = (index: number) => {
+        let keywordsCopy = keywords.slice();
+        keywordsCopy.splice(index, 1);
+        console.log("Index ", index," split array ", keywordsCopy);
+        addKeywords(keywordsCopy);
+    }
     
     return(
         <Paper elevation={2} sx={{padding:2}}>
@@ -36,9 +53,27 @@ export default function SocialMediaData(){
                 </Select>
             </FormControl>
             {displayKeywordSearch ? 
-            <Box>
-                <Typography variant="body1">Enter a keyword to search by</Typography>
-                <TextField variant='outlined' label="Keywords"></TextField>
+            <Box sx={{marginTop:2}}>
+                <Stack spacing={1} direction="row" sx={{marginBottom:1}}>
+                    {keywords.map((keyword, index) => {
+                        return (
+                            <Chip key={index} label={keyword} onClick={() =>{removeKeyword(index)}}/>
+                        );
+                    })}
+                </Stack>
+                
+                <Stack direction="row" spacing={1}>
+                    <TextField 
+                        variant='outlined' 
+                        label="Keywords" 
+                        value={keywordInput}
+                        onChange={(e) => setKeywordInput(e.target.value)}
+                        helperText="Enter a keyword or phrase to search by"
+                        onKeyDown={(e) => e.key === "Enter" ? addKeyword(): null}
+                    ></TextField>
+                    <Button variant="contained" sx={{height:"3.5rem"}} onClick={() => addKeyword()}>Add</Button>
+                </Stack>
+                
             </Box>
             : <></>
             }
