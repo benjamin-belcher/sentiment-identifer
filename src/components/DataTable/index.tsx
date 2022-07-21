a1f68e834c33494f5b637560cc399854b366c7db
BelchieBen <76218159+BelchieBen@users.noreply.github.com>
2022-07-21 21:57:28 +0100

    Added interface for data table

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
-export default function DataTable(props:any){
+interface DataTableProps{
+  "columns":
+  [
+    {
+      "field": string,
+      "headerName": string,
+      "width": number,
+    }
+  ],
+  "rows": 
+  [
+    {
+      // Empty object here as row data can be different
+    }
+  ]
+}
+
+export default function DataTable(props:DataTableProps){
     return(
         <DataGrid
-        rows={props.rows}
-        columns={props.columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
+            rows={props.rows}
+            columns={props.columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
       />
     )
 }
