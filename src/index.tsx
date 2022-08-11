76344845e3330f61b8cade99c5cead01be884678
benjamin.belcher <benjamin.belcher@ideagen.com>
2022-08-11 13:16:41 +0100

    These changes allow Netlify to handle url routes using browser router
    The redirects file will redirect the user when they refresh to the base page. Currently I use a hash router to keep the urls working but they dont look as clean. These changes can be applied in the future if needed.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider, createTheme} from '@mui/material';
 
 // This theme doesnt work for some reason
@@ -26,9 +26,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <ThemeProvider theme={theme}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ThemeProvider>
 );
 
